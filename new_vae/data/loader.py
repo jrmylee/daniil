205abbc3c9cc9874aae951484e861fb2b6bd904c
jrmylee <jrmylee@berkeley.edu>
2021-09-23 17:44:09 -0700

    batch

--- new_vae/data/loader.py
+++ new_vae/data/loader.py
@@ -53,5 +53,7 @@ def prepare_for_training(ds, shuffle_buffer_size=1024, batch_size=64):
     ds = ds.shuffle(buffer_size=shuffle_buffer_size)
     # Load and decode audio from file paths
     ds = ds.map(load_audio, num_parallel_calls=AUTOTUNE)
+    # Prepare batches
+    ds = ds.batch(batch_size, drop_remainder=True)
 
     return ds
