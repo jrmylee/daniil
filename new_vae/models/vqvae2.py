529109eb9c418f10f62d7a3043e1d67077fa0ba5
jrmylee <jrmylee@berkeley.edu>
2021-10-24 21:07:03 -0700

    tfp

--- new_vae/models/vqvae2.py
+++ new_vae/models/vqvae2.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 
 from tensorflow import keras
 from tensorflow.keras import layers
-import tensorflow_probability as tfp
 import tensorflow as tf
 
 class VectorQuantizer(layers.Layer):
