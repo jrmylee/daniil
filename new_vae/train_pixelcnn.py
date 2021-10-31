35ff2646ad53d7d546244da01a6afd50ec5c4996
jrmylee <jrmylee@berkeley.edu>
2021-10-31 11:46:17 -0700

    tf model

--- new_vae/train_pixelcnn.py
+++ new_vae/train_pixelcnn.py
@@ -1,9 +1,9 @@
 from loader import get_dataset
-import tensorflow as tf
-import tensorflow_datasets as tfds
-import tensorflow_probability as tfp
 import numpy as np
-from models.pixelcnn import PixelCNN
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tqdm import tqdm
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
@@ -14,7 +14,7 @@ def read_codes(item):
 
 def load_file(filepath):
     codes = tf.numpy_function(read_codes, [filepath], [tf.float32])
-    return (codes,)
+    return codes
 
 def split_data(ds, shuffle_buffer_size=1024, batch_size=64):
     test_ds = ds.take(200) 
@@ -33,44 +33,88 @@ def split_data(ds, shuffle_buffer_size=1024, batch_size=64):
     
     return train_ds, test_ds
 
+# The first layer is the PixelCNN layer. This layer simply
+# builds on the 2D convolutional layer, but includes masking.
+class PixelConvLayer(layers.Layer):
+    def __init__(self, mask_type, **kwargs):
+        super(PixelConvLayer, self).__init__()
+        self.mask_type = mask_type
+        self.conv = layers.Conv2D(**kwargs)
+
+    def build(self, input_shape):
+        # Build the conv2d layer to initialize kernel variables
+        self.conv.build(input_shape)
+        # Use the initialized kernel to create the mask
+        kernel_shape = self.conv.kernel.get_shape()
+        self.mask = np.zeros(shape=kernel_shape)
+        self.mask[: kernel_shape[0] // 2, ...] = 1.0
+        self.mask[kernel_shape[0] // 2, : kernel_shape[1] // 2, ...] = 1.0
+        if self.mask_type == "B":
+            self.mask[kernel_shape[0] // 2, kernel_shape[1] // 2, ...] = 1.0
+
+    def call(self, inputs):
+        self.conv.kernel.assign(self.conv.kernel * self.mask)
+        return self.conv(inputs)
+
+
+# Next, we build our residual block layer.
+# This is just a normal residual block, but based on the PixelConvLayer.
+class ResidualBlock(keras.layers.Layer):
+    def __init__(self, filters, **kwargs):
+        super(ResidualBlock, self).__init__(**kwargs)
+        self.conv1 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu"
+        )
+        self.pixel_conv = PixelConvLayer(
+            mask_type="B",
+            filters=filters // 2,
+            kernel_size=3,
+            activation="relu",
+            padding="same",
+        )
+        self.conv2 = keras.layers.Conv2D(
+            filters=filters, kernel_size=1, activation="relu"
+        )
+
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.pixel_conv(x)
+        x = self.conv2(x)
+        return keras.layers.add([inputs, x])
 
 dataset = get_dataset(ds_dir="/global/scratch/users/jrmylee/preprocessed/codes")
 train_ds, test_ds = split_data(dataset)
 
 top_input_size, bot_input_size = (128, 11, 1), (256, 22, 1)
+n_residual_blocks = 5
 
-tfd = tfp.distributions
-tfk = tf.keras
-tfkl = tf.keras.layers
+inputs = keras.Input(shape=top_input_size)
+x = PixelConvLayer(
+    mask_type="A", filters=128, kernel_size=7, activation="relu", padding="same"
+)(inputs)
 
-def image_preprocess(x):
-  x['image'] = tf.cast(x['image'], tf.float32)
-  return (x['image'],)  # (input, output) of the model
+for _ in range(n_residual_blocks):
+    x = ResidualBlock(filters=128)(x)
 
-# Define a Pixel CNN network
-dist = tfd.PixelCNN(
-    image_shape=top_input_size,
-    num_resnet=1,
-    num_hierarchies=2,
-    num_filters=32,
-    num_logistic_mix=5,
-    dropout_p=.3,
-)
+for _ in range(2):
+    x = PixelConvLayer(
+        mask_type="B",
+        filters=128,
+        kernel_size=1,
+        strides=1,
+        activation="relu",
+        padding="valid",
+    )(x)
 
-# Define the model input
-image_input = tfkl.Input(shape=top_input_size)
+out = keras.layers.Conv2D(
+    filters=1, kernel_size=1, strides=1, activation="sigmoid", padding="valid"
+)(x)
 
-# Define the log likelihood for the loss fn
-log_prob = dist.log_prob(image_input)
+pixel_cnn = keras.Model(inputs, out)
+adam = keras.optimizers.Adam(learning_rate=0.0005)
+pixel_cnn.compile(optimizer=adam, loss="binary_crossentropy")
 
-# Define the model
-model = tfk.Model(inputs=image_input, outputs=log_prob)
-model.add_loss(-tf.reduce_mean(log_prob))
-
-# Compile and train the model
-model.compile(
-    optimizer=tfk.optimizers.Adam(.001),
-    metrics=[])
+pixel_cnn.summary()
 
 checkpoint_filepath = "/global/home/users/jrmylee/projects/daniil/new_vae/saved_models/pixel_model_1"
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
@@ -83,4 +127,7 @@ callbacks = [
     model_checkpoint_callback
 ]
 
-model.fit(train_ds, epochs=10, verbose=True, callbacks=callbacks)
+pixel_cnn.fit(
+    x=train_ds, y=train_ds, batch_size=128, epochs=50, validation_split=0.1, verbose=2, callbacks=callbacks
+)
+
